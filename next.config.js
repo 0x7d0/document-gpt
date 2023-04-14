d8bd02f4442fe45952088b4cddda6cbc1d9ab89f
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-14 14:59:22 +0200

    one more time next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Remove the `fs` property from the `node` configuration
-    delete config.node.fs;
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty'
+      }
+    }
 
-    return config;
+    return config
   },
   env: {
     GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
-};
+}
