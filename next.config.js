fa736bc510c63966bd993c94018c1fa14a7862c1
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-14 16:00:38 +0200

    another fix of module.exports

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        global: true,
-        __dirname: false,
-        __filename: false,
-      };
-    }
-
-    return config;
-  },
+  target: "serverless",
   env: {
     GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
