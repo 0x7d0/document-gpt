d04250460305e738cfbb8eaf2ddd02c2519704d9
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-14 14:04:17 +0200

    updated index

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,16 @@ import DocumentAnalysis from "../components/DocumentAnalysis";
 
 export default function Home() {
   return (
-    <div>
-      <DocumentAnalysis />
+    <div className="min-h-screen bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500">
+      <div className="container mx-auto px-4 py-12">
+        <header className="text-center text-white mb-12">
+          <h1 className="text-5xl font-bold mb-4">Legal Document Analyzer</h1>
+          <p className="text-xl">
+            Upload a scanned PDF of a Polish legal document, and our AI will analyze its content.
+          </p>
+        </header>
+        <DocumentAnalysis />
+      </div>
     </div>
   );
 }
